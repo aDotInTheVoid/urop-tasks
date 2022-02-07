939690b1ef275cf34c36dd60ce76a2dc004c5f09
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2022-02-07 23:23:32 +0000

    Allocate from buffer to avoid cycles on dynamic linking

--- malloc_trace.c
+++ malloc_trace.c
@@ -1,13 +1,32 @@
- #define _GNU_SOURCE
+#define _GNU_SOURCE
+#include <dlfcn.h>
 #include <stdbool.h>
 #include <stdio.h>
- #include <dlfcn.h>
 
-typedef void*(*malloc_ptr)(size_t);
+typedef void *(*malloc_ptr)(size_t);
 
-void* malloc(size_t size) {
-	fprintf(stderr, "malloc(%zu)\n", size);
-	malloc_ptr libc_ptr = (malloc_ptr)dlsym(RTLD_NEXT, "malloc");
-       return (*libc_ptr)(size);	
+#define BUFFER_SIZE 1000000
+
+char buffer[BUFFER_SIZE];
+int buffer_offset = 0;
+
+size_t adjust_size(size_t size) { return size + 16 - (size % 16); }
+
+void *malloc(size_t size) {
+  fprintf(stderr, "malloc(%zu)", size);
+
+  void *ptr = &buffer;
+  size = adjust_size(size);
+  ptr += buffer_offset;
+  buffer_offset += size;
+
+  fprintf(stderr, " = %p\n", ptr);
+  fflush(stderr);
+  return ptr;
+  //      malloc_ptr libc_ptr = (malloc_ptr)dlsym(RTLD_NEXT, "malloc");
+  //     return (*libc_ptr)(size);
 }
 
+void free(void *ptr) {
+  // lmao
+}
