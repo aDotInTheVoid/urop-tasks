790cf78ebf6789e7012daa152ba8f1363acc02ca
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2022-02-08 00:10:30 +0000

    Only use buffer for mallocs by libdl

--- Makefile
+++ Makefile
@@ -4,5 +4,15 @@ all: malloc.so
 malloc.so: malloc_trace.c
 	gcc $< -o $@ -shared -fPIC
 
+obj/%: examples/%.c
+	gcc $< -o $@
+
 mt_seq: malloc.so
 	LD_PRELOAD=./malloc.so seq 1 10
+
+mt_alloc_allot: malloc.so obj/alloc_alot
+	LD_PRELOAD=./malloc.so obj/alloc_alot
+
+.PHONY: clean
+clean:
+	rm -f malloc.so obj/* a.out
--- malloc_trace.c
+++ malloc_trace.c
@@ -12,21 +12,48 @@ int buffer_offset = 0;
 
 size_t adjust_size(size_t size) { return size + 16 - (size % 16); }
 
-void *malloc(size_t size) {
-  fprintf(stderr, "malloc(%zu)", size);
+malloc_ptr libc_malloc = NULL;
+bool started = false;
 
+int id = 0;
+
+void *malloc(size_t size)
+{
+  int mid = id++;
+  fprintf(stderr, "%d: malloc(%zu)\n", mid, size);
+
+  if (libc_malloc != NULL)
+  {
+    void *ptr = (*libc_malloc)(size);
+    fprintf(stderr, "%d: Got %p with libc\n", mid, ptr);
+    return ptr;
+  }
+
+  // Ensure we only call dlsym once to avoid recursion
+  if (!started)
+  {
+    started = true;
+    fprintf(stderr, "%d: Initint libc\n", mid);
+    libc_malloc = dlsym(RTLD_NEXT, "malloc");
+    void *ptr = (*libc_malloc)(size);
+    fprintf(stderr, "%d: got %p (via libc init)\n", mid, ptr);
+    return ptr;
+  }
+
+  // Buffer allocation path
   void *ptr = &buffer;
   size = adjust_size(size);
   ptr += buffer_offset;
   buffer_offset += size;
 
-  fprintf(stderr, " = %p\n", ptr);
-  fflush(stderr);
+  fprintf(stderr, "%d: got %p (buffer)\n", mid, ptr);
+
   return ptr;
   //      malloc_ptr libc_ptr = (malloc_ptr)dlsym(RTLD_NEXT, "malloc");
   //     return (*libc_ptr)(size);
 }
 
-void free(void *ptr) {
+void free(void *ptr)
+{
   // lmao
 }
