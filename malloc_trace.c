c5106d8845b7d9cb08edacd7235070fedb9636de
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2022-06-01 23:44:20 +0100

    Trace number of malloc calls

--- malloc_trace.c
+++ malloc_trace.c
@@ -11,13 +11,17 @@
 void *(*libc_malloc)(size_t size) = NULL;
 
 void log_malloc(size_t size) {
-  static atomic_size_t count = 0;
-  size_t old_count = atomic_fetch_add(&count, size);
-  fprintf(stderr, "Allocation of %zu, total = %zu\n", size, old_count + size);
+  static atomic_size_t bytes_count = 0;
+  static atomic_size_t calls_count = 0;
+  size_t old_bytes = atomic_fetch_add(&bytes_count, size);
+  size_t old_calls = atomic_fetch_add(&calls_count, 1);
+  fprintf(stderr, "Allocation of %zu, total bytes = %zu, total calls = %zu\n",
+          size, old_bytes + size, old_calls + 1);
 }
 
 void *malloc(size_t size) {
-  if (libc_malloc == NULL) libc_malloc = dlsym(RTLD_NEXT, "malloc");
+  if (libc_malloc == NULL)
+    libc_malloc = dlsym(RTLD_NEXT, "malloc");
   void *ptr = (*libc_malloc)(size);
   if (ptr != NULL)
     log_malloc(size);
@@ -33,12 +37,14 @@ void *malloc(size_t size) {
 // https://www.gnu.org/software/libc/manual/html_node/Allocating-Cleared-Space.html
 void *calloc(size_t count, size_t eltsize) {
   void *value = reallocarray(NULL, count, eltsize);
-  if (value != 0) memset(value, 0, count * eltsize);
+  if (value != 0)
+    memset(value, 0, count * eltsize);
   return value;
 }
 
 void *realloc(void *old_ptr, size_t new_size) {
-  if (old_ptr == NULL) return malloc(new_size);
+  if (old_ptr == NULL)
+    return malloc(new_size);
   if (new_size == 0) {
     free(old_ptr);
     return NULL;
